6e11a63221b497b53f90cd6d46560b9e3270baea
IDungeon <57952984+IDungeon@users.noreply.github.com>
2020-01-30 10:45:24 +0100

    Create init_config.h

---
+++ init_config.h
@@ -0,0 +1,8 @@
+#ifndef INIT_CONFIG_H
+#define INIT_CONFIG_H
+
+void init_config1(char *configuration, parameters params);
+void init_config2(char *configuration, parameters params);
+void init_config3(char *configuration, parameters params);
+
+#endif
