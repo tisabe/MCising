544b0b156affc1d8d4c255997a48f5c612517a14
duezelbi <56878577+duezelbi@users.noreply.github.com>
2020-02-01 14:20:59 +0100

    changed data type of input to char in accordance to other functions

---
+++ observables.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+#include "structs.h"
+#include "geometry.h"
+#include "vmath.h"
+
+void hamiltonian(double *out, char *in, long int *neighbour, double B, long int N, unsigned int D, long int iteration){
+    double hamilton_config = 0;
+    L=ipow(N,D);
+    for (long int i=0; i<L; i++){
+        hamilton_config += (-1)*B*in[i];
+        for (int j=0; j<D; j++){
+            hamilton_config += (-1)*in[i]*(in[neighbour[2*i*D+2*j]]+in[neighbour[2*i*D+2*j+1]]);
+        }
+    }
+    out[iteration]=hamilton_config;
+}
+
+void magnetization((double *out, char *in, long int N, unsigned int D, long int iteration){
+    double magnetization_config = 0;
+    L=ipow(N,D);
+    for (long int i=0; i<L; i++){
+        hamilton_config += in[i];
+    }
+    out[iteration]=magnetization_config;
+}
