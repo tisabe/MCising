ab7df71e018d89ea436212f481f18b0277fdd4a1
duezelbi <56878577+duezelbi@users.noreply.github.com>
2020-02-01 15:22:04 +0100

    created observables.h

---
+++ observables.h
@@ -0,0 +1,2 @@
+void hamiltonian(double *out, char *in, long int *neighbour, double B, long int N, unsigned int D, long int iteration);
+void magnetization((double *out, char *in, long int N, unsigned int D, long int iteration);
