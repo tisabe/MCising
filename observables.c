683976f6cd79c83dbf02294809af7d439e78e4a0
Mahni Mueller <mulermah@pool1.physik.hu-berlin.de>
2020-02-03 13:38:46 +0100

    replaced N,D,L with struct parameters

--- observables.c
+++ observables.c
@@ -6,22 +6,20 @@
 #include "geometry.h"
 #include "vmath.h"
 
-void hamiltonian(double *out, char *in, long int *neighbour, double B, long int N, unsigned int D, long int iteration){
+void hamiltonian(double *out, char *in, long int *neighbour, long int iteration){
     double hamilton_config = 0;
-    long int L=ipow(N,D);
-    for (long int i=0; i<L; i++){
-        hamilton_config += (-1)*B*in[i];
-        for (int j=0; j<D; j++){
-            hamilton_config += (-1)*in[i]*(in[neighbour[2*i*D+2*j]]+in[neighbour[2*i*D+2*j+1]]);
+    for (long int i=0; i<params.L; i++){
+        hamilton_config += (-1)*params.B*in[i];
+        for (int j=0; j<params.D; j++){
+            hamilton_config += (-1)*in[i]*(in[neighbour[2*i*params.D+2*j]]+in[neighbour[2*i*params.D+2*j+1]]);
         }
     }
     out[iteration]=hamilton_config;
 }
 
-void magnetization(double *out, char *in, long int N, unsigned int D, long int iteration){
+void magnetization(double *out, char *in, long int iteration){
     double magnetization_config = 0;
-    long int L=ipow(N,D);
-    for (long int i=0; i<L; i++){
+    for (long int i=0; i<params.L; i++){
         magnetization_config += in[i];
     }
     out[iteration]=magnetization_config;
