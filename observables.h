9e7046f8e96b000ede180af9f86a58dbc1df3ada
Birkan Duezel <duezelbi@pool24.physik.hu-berlin.de>
2020-02-04 16:02:25 +0100

    updated observables.h to new hamiltonian function

--- observables.h
+++ observables.h
@@ -1,5 +1,5 @@
 #include "structs.h"
 
-double hamiltonian(char *in, parameters params, long int *neighbour);
+double hamiltonian(char *in, parameters params);
 double magnetization(char *in, parameters params);
 double auto_corr(double *obs, long int N, long int k, double mu);
