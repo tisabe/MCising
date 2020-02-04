7096f0626992a39b37e7e69bd9c194d0a69c24dc
Lennart Frohloff <frohlofl@pool23.physik.hu-berlin.de>
2020-02-04 15:38:20 +0100

    observables minor fixes

--- observables.h
+++ observables.h
@@ -1,5 +1,5 @@
 #include "structs.h"
 
-void hamiltonian(double *out, char *in, parameters params, long int *neighbour, long int iteration);
-void magnetization(double *out, char *in, parameters params, long int iteration);
+double hamiltonian(char *in, parameters params, long int *neighbour);
+double magnetization(char *in, parameters params);
 double auto_corr(double *obs, long int N, long int k, double mu);
