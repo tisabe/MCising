b9c5c8c7b39c5055f249f8e08ec2294f5e389d6f
tisabe <bechtelt@physik.hu-berlin.de>
2020-02-06 11:16:43 +0100

    new parameters tested

--- run_params.c
+++ run_params.c
@@ -10,7 +10,18 @@
 #include "init_config.h"
 #include "observables.h"
 
-#define num_params 12
+#define num_params 18
+
+unsigned long int float_to_uint(double f) {
+    if (f<0) {
+        f *= -1.0;
+    }
+    while (f != (int)f) {
+        f *= 10.0;
+    }
+    return (unsigned long int)f;
+}
+
 int main() {
     printf("starting sets...\n");
     parameters params;
@@ -19,12 +30,13 @@ int main() {
     params.D = 2;
     params.L = ipow(params.N,params.D);
 
-    long int steps = 5000;
+    long int steps = 50000;
 
     // we define the number of parameter sets that will be calculated and the parameters itself
     //int num_params = 12;
-    double beta_arr[num_params] = {0.1,0.3,0.5,0.7,0.1,0.3,0.5,0.7,0.1,0.3,0.5,0.7};
-    double B_arr[num_params] = {0.0,0.0,0.0,0.0,0.01,0.01,0.01,0.01,0.03,0.03,0.03,0.03};
+    double beta_arr[num_params] = {0.1,0.3,0.4,0.425,0.43,0.435,0.44,0.45,0.5,0.7,0.1,0.3,0.5,0.7,0.1,0.3,0.5,0.7};
+    double B_arr[num_params] = {0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.01,0.01,0.01,0.01,0.03,0.03,0.03,0.03};
+    // NOTE: for seed 42 in init_config, 45 in step_mc, beta=0.7, B=0.0, intersting stuff happens
 
     char *s = malloc(params.L * sizeof(char));
     double *h_vec = malloc(steps * sizeof(double));
@@ -37,13 +49,13 @@ int main() {
         init_config_rng(s, 42, params); // every chain will be started from the same starting configuration
         params.beta = beta_arr[ip];
         params.B = B_arr[ip];
-        printf("starting mc with params %d\n", ip);
+        printf("starting mc with params beta=%e, B=%e\n", params.beta, params.B);
         for(long int i=0; i<steps; i++) { // loop over steps
             h_vec[i] = hamiltonian(s, params);
             //printf("1\n");
             m_vec[i] = magnetization(s, params);
             //printf("2\n");
-            step_mc(s, ip + 42, params); // do the monte-carlo step with a different seed
+            step_mc(s, float_to_uint(params.beta)*float_to_uint(params.B), params); // do the monte-carlo step with a different seed
         }
         // generate the file name
         FILE *obs_file;
