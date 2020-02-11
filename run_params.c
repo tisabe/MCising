8a2a11e6dd5e9481a3807efcce0a6162f9e550cd
Lennart Frohloff <frohlofl@pool5.physik.hu-berlin.de>
2020-02-11 16:05:47 +0100

    notebook fixed

--- run_params.c
+++ run_params.c
@@ -30,12 +30,12 @@ int main() {
     params.D = 2;
     params.L = ipow(params.N,params.D);
 
-    long int steps = 50000;
+    long int steps = 5000;
 
     // we define the number of parameter sets that will be calculated and the parameters itself
     //int num_params = 12;
     double beta_arr[num_params] = {0.1,0.3,0.5,0.7,0.1,0.3,0.5,0.7,0.1,0.3,0.5,0.7,0.4,0.425,0.43,0.435,0.44,0.45};
-    double B_arr[num_params] = {0.0,0.0,0.0,0.01,0.01,0.01,0.01,0.03,0.03,0.03,0.03,0.0,0.0,0.0,0.0,0.0,0.0};
+    double B_arr[num_params] = {0.0,0.0,0.0,0.0,0.01,0.01,0.01,0.01,0.03,0.03,0.03,0.03,0.0,0.0,0.0,0.0,0.0,0.0};
     // NOTE: for seed 42 in init_config, 45 in step_mc, beta=0.7, B=0.0, intersting stuff happens
 
     char *s = malloc(params.L * sizeof(char));
@@ -55,7 +55,7 @@ int main() {
             //printf("1\n");
             m_vec[i] = magnetization(s, params);
             //printf("2\n");
-            step_mc(s, float_to_uint(params.beta)*float_to_uint(params.B), params); // do the monte-carlo step with a different seed
+            step_mc(s, float_to_uint(params.beta)+float_to_uint(params.B), params); // do the monte-carlo step with a different seed
         }
         // generate the file name
         FILE *obs_file;
